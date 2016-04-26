37ece93f1361e311f83f1bfba621746550062ce6
Dong Liu <doliu@microsoft.com>
2016-04-26 10:20:09 +0800

    add test conf.

--- tests/testUtil.js
+++ tests/testUtil.js
@@ -1,10 +1,17 @@
-var utils = require('azure-cli/lib/util/utils');
-var profile = require("azure-cli/lib/util/profile");
-var subscription = profile.current.getSubscription();
-var client = utils.createResourceManagerClient(subscription);
+// Testing util using default account.
+
+var conf        = require('./conf.json');
+var utils       = require('azure-cli/lib/util/utils');
+var profile     = require("azure-cli/lib/util/profile");
+var client      = utils.createResourceManagerClient(profile.current.getSubscription());
+
 
 exports.getResourceGroup = function(name, callback){
     client.resourceGroups.get(name, function(err, ca){
         
     })
+}
+
+exports.createResourceGroup = function(name, location, callback){
+    //client.resourceGroups
 }
