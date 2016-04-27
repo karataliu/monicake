33c0ee936d7d7a10312ecfe88f804d7740733bef
Dong Liu <doliu@microsoft.com>
2016-04-27 10:49:02 +0800

    async assertation.

--- tests/testSpec.js
+++ tests/testSpec.js
@@ -2,17 +2,55 @@ var testUtil  = require('./testUtils');
 var chai      = require("chai");
 var assert    = chai.assert;
 chai.use(require("chai-as-promised"));
-var Promise =require('bluebird');
+
+//var outputs = res.properties.outputs;
+//return createMonitoringServer(outputs.resourceGroup, outputs.prefix);
+
+function createMonitoringServer(rgName, prefix){
+  var template = require('../azuredeployMonitoringServer.json');
+  var templateParameters = {
+    "monitorVmName": {
+      "value": prefix + "mon"
+    },
+    "storageAccount": {
+      "value": prefix + "sto"
+    },
+    "virtualNetworkName":{
+      "value": prefix + "vnet"
+    },
+    "subnetName":{
+      "value": "default"
+    },
+    "password":{
+      "value": "testPass&"
+    }
+  };
+
+  return createDeployment(rgName, template, templateParameters);
+}
 
 describe('Group', function() {
-    it('CreateEnv', function (done) {
+    it('CreateEnv', function () {
       this.timeout(1000*250);
-      assert.isFulfilled(testUtil.createTestEnv()).notify(done);
+      var testEnv = testUtil.createTestEnv();
+      //testEnv.then(console.log);
+      //testEnv.then(function(dat){assert.equal('b1', dat.prefix);});
+      return Promise.all([
+        assert.isFulfilled(testEnv),
+        testEnv.then(function(dat){
+          assert(dat.prefix.toString().startsWith('2'), "not starts with expected");
+        }),
+        testEnv.then(console.log)
+      ]);
     });
 });
 
-describe('Work', function() {
-    it('w1', function () {
+xdescribe('Work', function() {
+    it('a1', function () {
       assert.equal(1,1);
+      //assert.equal(1,2);
+      it('b1',function(){
+        assert.equal(2,3);
+      });
     });
 });
--- tests/testUtils.js
+++ tests/testUtils.js
@@ -3,7 +3,6 @@
 var conf        = require('./conf.json');
 var utils       = require('azure-cli/lib/util/utils');
 var profile     = require("azure-cli/lib/util/profile");
-var Promise     = require('bluebird');
 var client      = utils.createResourceClient(profile.current.getSubscription());
 
 function createTestResourceGroup(){
@@ -51,8 +50,39 @@ function createTestEnvDeployment(rgName){
   return createDeployment(rgName, template, templateParameters);
 }
 
+function createTestOutput(rgName){
+  var template = require('./templates/testOutput.json');
+  return createDeployment(rgName, template, {});
+}
+
+function createTestOutput1(){
+  return { id: '/subscriptions/c4528d9e-c99a-48bb-b12d-fde2176a43b8/resourceGroups/doliumt2016-04-26T10-20-04.255Z/providers/Microsoft.Resources/deployments/testDeployment',
+  name: 'testDeployment',
+  properties: 
+   { provisioningState: 'Succeeded',
+     correlationId: 'f23cde7b-1d93-4dd8-b876-c492b90466b8',
+     outputs: { prefix: {"value":2}, resourceGroup: {"value":3} },
+     providers: [],
+     dependencies: [],
+     parameters: {},
+     mode: 'Incremental' } };
+}
+
+function createTestResourceGroup1()
+{
+  return Promise.resolve("doliumt2016-04-26T10-20-04.255Z");
+}
+
 exports.createTestEnv = function(){
-  return createTestResourceGroup().then(createTestEnvDeployment);
+  //return createTestResourceGroup().then(createTestEnvDeployment).then(function(res){
+  return createTestResourceGroup1().then(createTestOutput1).then(function(res){
+    var outputs = res.properties.outputs;
+    var g1={
+      "resourceGroup" : outputs.resourceGroup.value,
+      "prefix"        : outputs.prefix.value
+    };
+    return Promise.resolve(g1);
+  });
 };
 
 exports.listTestResourceGroups = function(){
