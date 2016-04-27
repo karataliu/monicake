878fc22aa78a2b7f90dac9fee934917f55a3a368
Dong Liu <doliu@microsoft.com>
2016-04-27 10:50:09 +0800

    fix delTestRG.

--- tests/delTestRG.js
+++ tests/delTestRG.js
@@ -1,6 +1,6 @@
 var testUtils   = require('./testUtils');
 
-testUtils.listTestResourceGroups().done(function(list){
+testUtils.listTestResourceGroups().then(function(list){
     for(var rg in list)
         console.log("azure group delete '%s' -q >/dev/null &",list[rg].name);
 });
