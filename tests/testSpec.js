1f1d1d0589ac020d82110d41743dd6b7e4eb4fb8
Dong Liu <doliu@microsoft.com>
2016-04-27 12:44:45 +0800

    complete steps test.

--- tests/testSpec.js
+++ tests/testSpec.js
@@ -4,7 +4,12 @@ var chai      = require("chai");
 var assert    = chai.assert;
 chai.use(require("chai-as-promised"));
 
-function createMonitoringServer(rgName, prefix){
+function createMonitoringServer(rgName, prefix, mock){
+  if(mock)
+    return Promise.resolve(
+    { serverInternalIp: '192.168.0.5',
+      serverPublicEndpoint: 'http://doliumtmolvecaqnwzmsmon.southeastasia.cloudapp.azure.com/zab/' });
+
   var template = require('../azuredeployMonitoringServer.json');
   var templateParameters = {
     "monitorVmName": {
@@ -34,9 +39,36 @@ function createMonitoringServer(rgName, prefix){
     });
 }
 
-describe('Steps Test', function() {
+function createMonitoringAgentsByVnet(rgName, prefix, serverIp, mock){
+  if(mock){
+    return Promise.resolve();
+  }
+  
+  var template = require('../azuredeployMonitoringAgentByVnet.json');
+  var templateParameters = {
+    "virtualNetworkName":{
+      "value": prefix + "vnet"
+    },
+    "subnetName":{
+      "value": "default"
+    },
+    "serverIp":{
+      "value": serverIp
+    }
+  };
+
+  return testUtils.createDeployment(rgName, template, templateParameters)
+    .then(function(res){
+      console.log(res);
+    });
+}
+
+describe('Step Test', function() {
   var resourceGroup;
   var prefix;
+
+  var serverInternalIp;
+  var serverPublicEndpoint;
 
   it('CreateTestEnv', function () {
     this.timeout(1000*250);
@@ -55,11 +87,27 @@ describe('Steps Test', function() {
   it('CreateMonitoringServer', function () {
     assert(prefix, "Prefix should not be empty");
     assert(resourceGroup, "resourceGroup should not be empty");
-    this.timeout(1000*550);
-    var t1 = assert.isFulfilled(createMonitoringServer(resourceGroup, prefix));
+    this.timeout(1000*650);
+    var t1 = assert.isFulfilled(createMonitoringServer(resourceGroup, prefix,1));
     return Promise.all([
-      t1.then(console.log)
+      t1.then(console.log),
+      t1.then(function(dat){
+        serverInternalIp = dat.serverInternalIp;
+        serverPublicEndpoint = dat.serverPublicEndpoint;
+      })
     ]);
+  });
+
+  it('CreateMonitoringAgents', function () {
+    assert(serverInternalIp, "serverInternalIp should not be empty");
+    assert(serverPublicEndpoint, "serverPublicEndpoint should not be empty");
+    this.timeout(1000*550);
+    var t1 = assert.isFulfilled(createMonitoringAgentsByVnet(resourceGroup, prefix, serverInternalIp,1));
+    return t1;
+  });
+
+  it('VerifyPage', function(){
+    
   });
 });
 
